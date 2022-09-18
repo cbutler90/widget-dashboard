9dc38aa36175ff33b60c95a107b922d19b35f57c
Christine Butler <37420934+cbutler90@users.noreply.github.com>
2022-09-18 15:06:08 -0400

    adding files

---
+++ src/components/widgets/Schedule.vue
@@ -0,0 +1,83 @@
+<template>
+  <WidgetContainer title="Today's Schedule">
+    <div class="col-12 px-0 pb-0">
+            <div class="append">
+                <FullCalendar :events="events" :options="options" />
+            </div>
+        </div>
+        <ScrollPanel class="w-full schedule" style="max-height: 185px; min-height: 94px;">
+            <ScheduleCard />
+        </ScrollPanel>
+        <a href="#" class="capri">View Full Schedule</a>
+  </WidgetContainer>
+</template>
+
+<script>
+  import '@fullcalendar/core/vdom';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+  import interactionPlugin from '@fullcalendar/interaction';
+  import ScheduleCard from './ScheduleCard.vue'
+  import WidgetContainer from '@/components/WidgetContainer.vue'
+
+export default {
+  components: {
+    ScheduleCard,
+    WidgetContainer
+  },
+  data() {
+        return {
+            eventDialog: false,
+            clickedEvent: null,
+            changedEvent: { title: '', start: null, end: '', allDay: null },
+            options: {
+                plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+                defaultDate: '2021-07-01',
+                headerToolbar: {
+                    left: 'prev',
+                    center: 'title',
+                    right: 'next',
+                },
+                editable: true,
+                eventClick: e => {
+                    this.eventDialog = true;
+                    this.clickedEvent = e.event;
+                    this.changedEvent.title = this.clickedEvent.title;
+                    this.changedEvent.start = this.clickedEvent.start;
+                    this.changedEvent.end = this.clickedEvent.end;
+                },
+            },
+            events: null,
+        }
+    },
+    methods: {
+        findIndexById(id) {
+            let index = -1;
+            for (let i = 0; i < this.events.length; i++) {
+                if (this.events[i].id === id) {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        },
+        save() {
+            this.eventDialog = false;
+            this.clickedEvent.setProp('title', this.changedEvent.title);
+            this.clickedEvent.setStart(this.changedEvent.start);
+            this.clickedEvent.setEnd(this.changedEvent.end);
+            this.clickedEvent.setAllDay(this.changedEvent.allDay);
+            this.changedEvent = { title: '', start: null, end: '', allDay: null };
+        },
+        reset() {
+            this.changedEvent.title = this.clickedEvent.title;
+            this.changedEvent.start = this.clickedEvent.start;
+            this.changedEvent.end = this.clickedEvent.end;
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/widgets/ScheduleCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- @TODO:
+          figure out filter on events data (start,end)
+  -->
+  <ul>
+      <li v-for="event in filteredEvents" :key="event.id" class="border-l-5" :class="statusChecker(event)">
+          <div class="schedule-header">
+              <h6 class="text-lg letter-s-normal">{{ event.title }}</h6>
+          </div>
+          <div class="schedule-content flex justify-content-between align-items-center">
+              <div class="flex flex-column">
+                  <a href="#" v-if="event.cta">{{ event.cta }}</a>
+                  <span class="participants" v-if="event.numOfParticipants">{{ event.numOfParticipants }} participants</span>
+              </div>
+              <span class="text-base time">{{ event.begin }} - {{ event.close }}</span>
+          </div>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          events: [
+            {
+              id: 1,
+              title: 'test',
+              cta: 'Join Session',
+              numOfParticipants: 4,
+              start: '2022-09-09T16:00:00',
+              end: '2022-09-10T16:00:00',
+              begin: '12:00',
+              close: '16:00',
+            }
+          ],
+      };
+  },
+  methods: {
+      statusChecker(event) {
+          let now = new Date();
+          let start = new Date(event.start);
+          let status = start < now ? 'past' : 'upcoming';
+          if (start.getUTCFullYear() == now.getUTCFullYear() && start.getUTCMonth() == now.getUTCMonth() && start.getUTCDate() == now.getUTCDate()) {
+              status = 'today';
+          }
+          return status;
+      },
+  },
+  computed: {
+      filteredEvents() {
+          return this.events.filter(event => this.statusChecker(event) !== 'past');
+      },
+  },
+};
+</script>
+
+<style></style>
